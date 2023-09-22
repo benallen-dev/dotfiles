b83e9e28a7c79ec0130a79ea71fbbd1bfb650fd5
Ben Allen <ben@benallen.dev>
2023-09-22 19:21:14 +0200

    Add ksnip to i3, wacom setup, README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Dotfiles
+
+## Submodules
+- The config for nvim is currently a seperate repo: https://github.com/benallen-dev/nvim-config
+- alacritty/themes refers to https://github.com/alacritty/alacritty-theme
+
+## Configuration
+- alacritty
+- git
+- i3
+- lf
+- neofetch
+- neovim
+- polybar
+- tmux
+
+
+
+## Special guest
+- wacom, contains persistent config for One by Wacom tablet I have and is not copied into ~/.config by `install.sh`
---
+++ wacom/wacom-config.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# NOTE: For this to work, you need to:
+# - copy or symlink 99-wacom.rules to /etc/udev/rules.d/
+# - copy or symlink wacom.service to /.config/systemd/user/
+# - start the service with systemctl --user start wacom.service
+# - enable the service with systemctl --user enable wacom.service
+#
+# Also note that this is really OS-specific so it isn't included in
+# the install.sh script in the dotfiles root.
+
+for i in $(seq 10); do
+    if xsetwacom list devices | grep -q Wacom; then
+        break
+    fi
+    sleep 1
+done
+
+list=$(xsetwacom list devices)
+pad=$(echo "${list}" | awk '/pad/{print $9}')
+stylus=$(echo "${list}" | awk '/stylus/{print $9}')
+eraser=$(echo "${list}" | awk '/eraser/{print $9}')
+
+if [ -z "${stylus}" ]; then
+    exit 0
+fi
+
+# configure the buttons on ${stylus} with your xsetwacom commands...
+xsetwacom set ${stylus} Button 2 "pan"
+xsetwacom set ${stylus} "PanScrollThreshold" 150
+
