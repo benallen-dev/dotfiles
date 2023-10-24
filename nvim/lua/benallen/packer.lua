ba02e4dab3de848db05e83a46cdbb176b6a9d92a
Ben Allen <ben@benallen.dev>
2023-10-24 23:11:35 +0200

    Remove vim-be-good, disable nvim-cmp for now

--- nvim/lua/benallen/packer.lua
+++ nvim/lua/benallen/packer.lua
@@ -22,7 +22,6 @@ return require('packer').startup(function(use)
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
 	use('theprimeagen/harpoon')
-	use('theprimeagen/vim-be-good')
 
 	use('mbbill/undotree')
 
