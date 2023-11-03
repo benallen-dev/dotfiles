519f474ef7bc44a85c2c8f79402160874b540f59
Ben Allen <ben@benallen.dev>
2023-11-03 20:33:51 +0100

    [neovim] Add yank highlighting

--- nvim/lua/benallen/init.lua
+++ nvim/lua/benallen/init.lua
@@ -1,3 +1,19 @@
 require("benallen.settings")
 require("benallen.remap")
 
+-- Shamelessly stolen from ThePrimagen. Again.
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
