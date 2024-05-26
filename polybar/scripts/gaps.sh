606c095baf88a82f360742b41ca18f628282d485
Ben Allen <ben@benallen.dev>
2024-05-26 12:16:15 +0200

    [i3] Add outer gap keybind, improve gap display

--- polybar/scripts/gaps.sh
+++ polybar/scripts/gaps.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-start_time=$(($(date +%s%N)/1000))
+# start_time=$(($(date +%s%N)/1000))
 
-if [ $(i3-msg -t get_binding_state | jq -r '.name') != "default" ]; then
-	gaps=$(i3-msg -t get_tree | jq '..|.gaps? | select(.!=null) | select(.inner!=0) | .inner')
+if [[ $(i3-msg -t get_binding_state | jq -r '.name') != "default"  ||  $1 != "" ]]; then
+	gaps=$(i3-msg -t get_tree | jq '.nodes[] | select(.name == "DP-5").nodes[] | select(.name == "content").nodes[] | select(.fullscreen_mode == 1) | .gaps | {"inner": .inner, "outer":.outer'} | tr '\n' ' ')
 	echo ${gaps:-"default"} > /tmp/gaps
 else
 	echo "" > /tmp/gaps
 fi
 
- end_time=$(($(date +%s%N)/1000))
- duration=$(($end_time - $start_time))
- echo $duration μs
+ # end_time=$(($(date +%s%N)/1000))
+ # duration=$(($end_time - $start_time))
+ # echo $duration μs
