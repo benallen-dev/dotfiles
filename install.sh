9cbf2539c059029ac4f4ff735466af469765cd12
Ben Allen <ben@benallen.dev>
2024-05-28 08:52:57 +0200

    Change polybar to HDMI-0, add Rofi config

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ mv ~/.config/lf ~/.config/lf.bak 2>/dev/null || true
 mv ~/.config/neofetch ~/.config/neofetch.bak 2>/dev/null || true
 mv ~/.config/nvim ~/.config/nvim.bak 2>/dev/null || true
 mv ~/.config/polybar ~/.config/polybar.bak 2>/dev/null || true
+mv ~/.config/rofi ~/.config/rofi.bak 2>/dev/null || true
 mv ~/.config/tmux ~/.config/tmux.bak 2>/dev/null || true
 
 # Create symlinks to dotfiles
@@ -18,5 +19,6 @@ ln -s -n ~/.dotfiles/lf ~/.config/lf
 ln -s -n ~/.dotfiles/neofetch ~/.config/neofetch
 ln -s -n ~/.dotfiles/nvim ~/.config/nvim
 ln -s -n ~/.dotfiles/polybar ~/.config/polybar
+ln -s -n ~/.dotfiles/rofi ~/.config/rofi
 ln -s -n ~/.dotfiles/tmux ~/.config/tmux
 
--- polybar/scripts/gaps.sh
+++ polybar/scripts/gaps.sh
@@ -3,7 +3,7 @@
 # start_time=$(($(date +%s%N)/1000))
 
 if [[ $(i3-msg -t get_binding_state | jq -r '.name') != "default"  ||  $1 != "" ]]; then
-	gaps=$(i3-msg -t get_tree | jq '.nodes[] | select(.name == "DP-5").nodes[] | select(.name == "content").nodes[] | select(.fullscreen_mode == 1) | .gaps | {"inner": .inner, "outer":.outer'} | tr '\n' ' ')
+	gaps=$(i3-msg -t get_tree | jq '.nodes[] | select(.name == "HDMI-0").nodes[] | select(.name == "content").nodes[] | select(.fullscreen_mode == 1) | .gaps | {"inner": .inner, "outer":.outer'} | tr '\n' ' ')
 	echo ${gaps:-"default"} > /tmp/gaps
 else
 	echo "" > /tmp/gaps
