d8d9f8572936fbbb7a1574e83e3c8840147af75e
Ben Allen <ballen@anwb.nl>
2024-06-28 08:33:18 +0200

    [nvim] improve search keybinds

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,16 +1,26 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', function()
+
+-- (f)ile search
+vim.keymap.set('n', '<leader>f', function()
 	builtin.find_files({  find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }});
-	-- builtin.find_files({ hidden = true });
 end)
-vim.keymap.set('n', '<leader>j', function()
-	builtin.find_files({  find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }});
-	-- builtin.find_files({ hidden = true });
+-- (g)git search, not used as often therefore double key is ok
+vim.keymap.set("n", "<leader>gg", builtin.git_files, {})
+
+-- search (i)nside file
+vim.keymap.set('n', '<leader>i', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+-- Old (p)roject (s)earch
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-vim.keymap.set("n", "<leader>f", builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>pf', function()
+	builtin.find_files({  find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }});
+	--	builtin.find_files({ hidden = true });
+end)
 
