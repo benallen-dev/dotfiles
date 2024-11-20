b4722fef8cacc25a6a535b1c75ce2bfd3a851387
Ben Allen <ballen@anwb.nl>
2024-11-20 17:58:27 +0100

    move stuff around so we can try using stow

--- scripts/i3.sh
+++
@@ -1,6 +0,0 @@
-fixmonitors() {
-	xrandr --output HDMI-0 --off
-	xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left --output DP-4 --primary --mode 3440x1440 --pos 1080x0 --rotate normal
-	feh --bg-fill /usr/share/sddm/themes/chili/assets/background.jpg
-}
-
