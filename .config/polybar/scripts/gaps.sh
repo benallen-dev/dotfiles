7d2620e349efd9a3fca55140098984d58b441da1
Ben Allen <ben@benallen.dev>
2025-04-26 08:27:40 +0200

    Tweak i3 setup

--- .config/polybar/scripts/gaps.sh
+++ .config/polybar/scripts/gaps.sh
@@ -4,9 +4,9 @@
 
 if [[ $(i3-msg -t get_binding_state | jq -r '.name') != "default"  ||  $1 != "" ]]; then
 	gaps=$(i3-msg -t get_tree | jq '.nodes[] | select(.name == "HDMI-0").nodes[] | select(.name == "content").nodes[] | select(.fullscreen_mode == 1) | .gaps | {"inner": .inner, "outer":.outer'} | tr '\n' ' ')
-	echo ${gaps:-"default"} > /tmp/gaps
+	echo ${gaps:-"default"} #> /tmp/gaps
 else
-	echo "" > /tmp/gaps
+	echo "" #> /tmp/gaps
 fi
 
  # end_time=$(($(date +%s%N)/1000))
