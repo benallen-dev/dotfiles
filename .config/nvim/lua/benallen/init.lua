3ca395f4ac0e6cf578e60df47e6750c7675ed0ed
Ben Allen <ben@benallen.dev>
2025-05-15 16:54:55 +0200

    Auto-close quickfix list after jumping to a file

--- .config/nvim/lua/benallen/init.lua
+++ .config/nvim/lua/benallen/init.lua
@@ -7,16 +7,27 @@ require("benallen.godot")
 -- Shamelessly stolen from ThePrimagen. Again.
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
 
+local yank_group = augroup('HighlightYank', {})
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+	group = yank_group,
+	pattern = '*',
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = 'IncSearch',
+			timeout = 40,
+		})
+	end,
 })
 
+local close_quickfix = augroup('CloseQuickfix', {})
+autocmd('FileType', {
+	group = close_quickfix,
+	pattern = 'qf',
+	callback = function()
+		vim.keymap.set("n", "<CR>", function()
+			vim.cmd("cc")
+			vim.cmd("cclose")
+		end)
+	end
+})
