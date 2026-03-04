1ced8841f7dc93b74f993d26fa4957df61f61060
Ben Allen <ben@benallen.dev>
2026-03-04 22:27:45 +0100

    [lazy] add highlight-on-yank autocmd

--- .config/nvim-new/lua/config/autocmds.lua
+++ .config/nvim-new/lua/config/autocmds.lua
@@ -1,1 +1,13 @@
 print("Hello world from autocmds.lua")
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+	group = vim.api.nvim_create_augroup('HighlightYank', {}),
+	pattern = '*',
+	callback = function()
+		vim.hl.on_yank({
+			higroup = 'IncSearch',
+			timeout = 40,
+		})
+	end,
+})
+
