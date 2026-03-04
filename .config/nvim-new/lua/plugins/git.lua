8a737623651ebd6f73f495969983102572843315
Ben Allen <ben@benallen.dev>
2026-03-04 21:40:54 +0100

    lazy-load fugitive

--- .config/nvim-new/lua/plugins/git.lua
+++ .config/nvim-new/lua/plugins/git.lua
@@ -1,15 +1,11 @@
 return {
 	{
 		'tpope/vim-fugitive',
-		config = function()
-			vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
-			vim.keymap.set("n", "<leader>gf", function()
-				vim.cmd("Git pull")
-			end, { desc = "Git pull" })
-			vim.keymap.set("n", "<leader>gp", function()
-				vim.cmd("Git push")
-			end, { desc = "Git push" })
-		end
+		keys= {
+			{ "<leader>gs", vim.cmd.Git, desc = "Git status" },
+			{ "<leader>gf", function() vim.cmd("Git pull") end,  desc = "Git pull" },
+			{ "<leader>gp", function() vim.cmd("Git push") end,  desc = "Git push" }
+		}
 	},
 
 	{
