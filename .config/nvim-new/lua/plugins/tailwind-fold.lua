a04bb4171cd4da65deee076cee8e304d09e629d7
Ben Allen <ben@benallen.dev>
2026-03-04 21:41:07 +0100

    [lazy] add tailwind-fold

---
+++ .config/nvim-new/lua/plugins/tailwind-fold.lua
@@ -0,0 +1,7 @@
+return {
+	'razak17/tailwind-fold.nvim',
+	dependencies = { 'nvim-treesitter/nvim-treesitter' },
+	opts = {
+		ft = { 'html', 'typescriptreact', 'php', 'templ', 'svelte', 'astro', 'vue' }
+	}
+}
