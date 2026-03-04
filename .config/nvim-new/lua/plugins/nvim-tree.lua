886c6ad020ab7188023b58d2b8b25de0c105d5e0
Ben Allen <ben@benallen.dev>
2026-03-04 16:35:02 +0100

    [lazy] add nvim-tree

---
+++ .config/nvim-new/lua/plugins/nvim-tree.lua
@@ -0,0 +1,112 @@
+-- TODO: recalculate these when the window resizes?
+local gwidth = vim.api.nvim_list_uis()[1].width
+local gheight = vim.api.nvim_list_uis()[1].height
+local width = math.floor(gwidth * 0.8)
+local height = math.floor(gheight * 0.8)
+
+return {
+	{
+		'nvim-tree/nvim-tree.lua',
+		lazy = false,
+		keys = {
+			{ "<leader>b", vim.cmd.NvimTreeToggle },
+			{ "<C-b>", vim.cmd.NvimTreeFocus }
+		},
+		opts = {
+			sort_by = "case_sensitive",
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				}
+			},
+			view = {
+				relativenumber = true,
+				float = {
+					enable = true,
+					open_win_config = {
+						relative = "editor",
+						width = width,
+						height = height,
+						row = (gheight - height) * 0.4,
+						col = (gwidth - width) * 0.5,
+					}
+				}
+			},
+			renderer = {
+				root_folder_label = false,
+				group_empty = true,
+				indent_markers = {
+					enable = true,
+					inline_arrows = true,
+					icons = {
+						corner = "└",
+						edge = "│",
+						item = "│",
+						bottom = "─",
+						none = " ",
+					},
+				},
+				icons = {
+					web_devicons = {
+						file = {
+							enable = true,
+							color = true,
+						},
+						folder = {
+							enable = false,
+							color = true,
+						}
+					},
+					glyphs = {
+						default = "",
+						symlink = "",
+						bookmark = "󰆤",
+						modified = "●",
+						folder = {
+							arrow_closed = "",
+							arrow_open = "",
+							default = "",
+							open = "",
+							empty = "",
+							empty_open = "",
+							symlink = "",
+							symlink_open = "",
+						},
+						git = {
+							unstaged = "󰏫",
+							staged = "✓",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
+						},
+					},
+				},
+
+			},
+			modified = {
+				enable = true,
+				show_on_dirs = true,
+				show_on_open_dirs = true,
+			},
+			update_focused_file = {
+				enable = true,
+				update_root = false,
+				ignore_list = {},
+			},
+			filters = {
+				dotfiles = false,
+				git_ignored = false,
+				custom = {
+					"^\\.git$",
+				}
+			},
+		}
+	},
+
+	{
+		'nvim-tree/nvim-web-devicons',
+		opts = {}
+	}
+}
