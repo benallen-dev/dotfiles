a940344442ff4ffadffa3047c599efaab1e53add
Ben Allen <ben@benallen.dev>
2026-03-05 15:43:58 +0100

    [lazy] add a few missing keybinds

--- .config/nvim-new/lua/config/keymaps.lua
+++ .config/nvim-new/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
+vim.keymap.set("i", "jj", "<ESC>")
+
+vim.keymap.set({"n", "v"}, "<leader>y", "\"+y", { desc = "Yank into system clipboard" })
 
 vim.keymap.set({'n', 'i', 'v'}, '<S-CR>', '<Nop>', { desc = "Disable shift-enter from which-key" })
 
+-- Belongs elsewhere but here for now
+vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.code_action() end, {desc = "List code actions"})
