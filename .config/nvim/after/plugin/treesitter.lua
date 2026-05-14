2cd6bd68ec7230fd84cd29e06fdfa6693956500d
Ben Allen <ben@benallen.dev>
2026-05-14 15:25:32 +0200

    nvim updates

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -18,7 +18,7 @@ require'nvim-treesitter.configs'.setup {
 
 	highlight = {
 		enable = true,
-		disable = {"markdown"},
+		-- disable = {"markdown"},
 
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
